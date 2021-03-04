74982630710ad6ac85f57f4a76339a8df1d28027
Alvaro Gamero <gamerojtm@gmail.com>
2021-03-04 22:26:50 +0100

    Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { SentimentComponent } from './sentiment/sentiment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    HomeComponent,
+    SentimentComponent
   ],
   imports: [
     BrowserModule,
