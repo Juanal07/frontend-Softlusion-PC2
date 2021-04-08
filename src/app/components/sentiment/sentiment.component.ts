ffbbb94855c93c6cd342171c85ef6c078ff0c3a2
Juan Alberto Raya <jjrr1307@gmail.com>
2021-04-08 22:48:48 +0200

    sentiment.ts tipado + Prettier

--- src/app/components/sentiment/sentiment.component.ts
+++ src/app/components/sentiment/sentiment.component.ts
@@ -1,37 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { SentimentService } from '../../services/sentiment.service';
-import {Sentiment} from "../../models/sentiment.model";
-
+import { Sentiment } from '../../models/sentiment.model';
 
 @Component({
   selector: 'app-sentiment',
   templateUrl: './sentiment.component.html',
-  styleUrls: ['./sentiment.component.scss']
+  styleUrls: ['./sentiment.component.scss'],
 })
 export class SentimentComponent implements OnInit {
+  respuesta: string = '';
+  textArea: string = '';
+  sentiment: Sentiment = new Sentiment();
 
-  respuesta='';
-  textArea='';
-  sentiment = new Sentiment;
+  constructor(private sentimentService: SentimentService) {}
 
-  constructor(private sentimentService : SentimentService) { }
+  ngOnInit() {}
 
-  ngOnInit() {
-
+  viewSentiment() {
+    this.sentiment['text'] = this.textArea;
+    this.sentimentService.postAPIData(this.sentiment).subscribe(
+      (response) => {
+        console.log('response is ', response);
+        this.respuesta = response['result'];
+      },
+      (error) => {
+        console.log('error is ', error);
+      }
+    );
   }
-
-  viewSentiment(){
-    this.sentiment["text"]=this.textArea;
-
-    this.sentimentService.postAPIData(this.sentiment).subscribe((response)=>{
-      console.log('response is ', response)
-
-      this.respuesta=response["result"];
-
-    },(error) => {
-      console.log('error is ', error)
-    })
-
-  }
-
 }
