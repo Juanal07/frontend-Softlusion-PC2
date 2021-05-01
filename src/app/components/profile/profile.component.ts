f99c5c8a10816c376f64c813dd60d8eb8d35f4e6
Juan Alberto Raya <jjrr1307@gmail.com>
2021-05-02 00:55:26 +0200

    profile+user model

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
+  user: User;
+  name: string;
+  email: string;
+  old_psw: string;
+  new_psw: string;
+  repeat_psw: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.name = 'Juan';
+    this.email = 'jjrr1307@gmail.com';
   }
 
+  updateName() {
+    console.log(this.name);
+  }
+
+  updatePsw() {}
 }
