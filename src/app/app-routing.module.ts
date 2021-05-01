28531e8b436ededb57880562c6276dafb78aff5e
Juan Alberto Raya <jjrr1307@gmail.com>
2021-05-01 14:13:15 +0200

    vista profile y modifico metodo navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { SentimentComponent } from './components/sentiment/sentiment.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch:"full"},
-  { path: 'analisis', component: SentimentComponent, pathMatch:"full"}
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'analisis', component: SentimentComponent, pathMatch: 'full' },
+  { path: 'profile', component: ProfileComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { BuscadorComponent } from './components/buscador/buscador.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
@@ -24,8 +25,15 @@ import { BuscadorComponent } from './components/buscador/buscador.component';
     LoginComponent,
     RegisterComponent,
     BuscadorComponent,
+    ProfileComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, NgbModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NgbModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
