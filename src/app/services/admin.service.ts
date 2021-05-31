b6f8ca499f89e2c9a5638642c3179863f6d9011a
Alvaro Gamero <gamerojtm@gmail.com>
2021-05-31 14:11:13 +0200

    tablas administrador

---
+++ src/app/services/admin.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { endpoints } from '../../environments/endpoints';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+  constructor(private http: HttpClient) {}
+
+  getUsers() {
+    return this.http.get(`${endpoints.admin}/user-all`);
+  }
+
+  getMunicipiosMasBuscados() {
+    return this.http.get(`${endpoints.admin}/muniBuscados`);
+  }
+
+  getRankingActivos() {
+    return this.http.get(`${endpoints.admin}/rankingsActivos`);
+  }
+
+
+}
